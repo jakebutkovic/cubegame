e2ae0725f01ff8f1a03776242d652bf4cc09fa83
jakebutkovic <jakebutkovic@gmail.com>
2021-03-29 23:32:14 -0400

    format

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,19 +1,21 @@
 
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour{
+public class PlayerCollision : MonoBehaviour
+{
 
     public PlayerMovement movement; // a reference to our PlayerMovement script
-    
 
-    void OnCollisionEnter(Collision collisionInfo){
-       
-        if(collisionInfo.collider.tag == "Obstacle")
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
-            
+
         }
-       
+
     }
 }
